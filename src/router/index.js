4fe15ef2d60d1086b2718d7fab521d353f8c89aa
龙翔 <1364474091@qq.com>
2023-03-12 16:02:32 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { createApp } from 'vue'
+import App from '../App.vue'
+// import HelloWorld from "../components/HelloWorld.vue"
+// import TheWelcome from "../components/TheWelcome.vue"
+
+
+
+
+
+const routers=[
+	{ path: "/", redirect: "/home" },
+	{
+		path:'/HelloWorld/:id',
+		name:"HelloWorld",
+		component: () => import('../components/HelloWorld.vue'),
+		props:true
+	},
+	{
+		path:'/TheWelcome',
+		name:"TheWelcome",
+		component: () => import('../components/TheWelcome.vue'),
+		children:[
+		      // {
+		      //   //二级导航的路径不要加/
+		      //   path:'me',
+		      //   component:()=>import('/views/AboutSub/Aboutme.vue')
+		      // },
+		      // {
+		      //   path:'info',
+		      //   component:()=>import('/views/AboutSub/Aboutinfo.vue')
+		      // }
+		    ]
+	}
+]
+const router=createRouter({
+
+        history: createWebHashHistory(),
+        routes: routers,
+
+})
+
+export default router
